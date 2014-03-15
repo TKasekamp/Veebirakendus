384678050aef6314951a5863616c673dd067ddc8
unknown <sander.tiganik@gmail.com>
2014-03-15 14:22:26 +0200

    HTTPS vol 3

--- src/main/webapp/script/httpsRedirect.js
+++ src/main/webapp/script/httpsRedirect.js
@@ -1,4 +1,6 @@
 function httpsRedirect(){
 	if (window.location.protocol != "https:")
 		window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-}
+};
+
+httpsRedirect();
