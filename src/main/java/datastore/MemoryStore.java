761f8cdc37f1db26f929ba9af2d06faaf3ba20b8
TKasekamp <toniskasekamp@gmail.com>
2014-03-16 12:26:57 +0200

    Database disabled by default
    Set MemoryStore.USE_DATABASE to true to enable. Should be done before
    Heroku deployment

--- src/main/java/datastore/MemoryStore.java
+++ src/main/java/datastore/MemoryStore.java
@@ -1,51 +1,93 @@
 package datastore;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.hibernate.Session;
+
 import data.CodeItem;
 import util.HibernateUtil;
 
 public class MemoryStore implements CodeDataProvider {
 
-	private Session session = HibernateUtil.currentSession();
+	private Session session;
+	private Map<Integer, CodeItem> items;
+	private int codeCounter; // useless
+
+	public static boolean USE_DATABASE = false; // CHANGE THIS WHEN DEPLOYING
 
 	public MemoryStore() {
 
+		if (USE_DATABASE) {
+			session = HibernateUtil.currentSession();
+
+		} else {
+			items = new HashMap<>();
+			items.put(1, new CodeItem(1, "hello",
+					"public static void Hello(String s);", "java", "Public"));
+			items.put(2, new CodeItem(2, "bla", "print(\"bla\")", "python",
+					"Public"));
+			items.put(3,
+					new CodeItem(3, "haha", "Hello::Hello", "c", "Private"));
+			codeCounter = 4;
+		}
 	}
 
 	@Override
 	public void addCode(CodeItem item) {
 
-		item.setSaveDate(new Date());
-		item.setExpireDate(new Date());
+		if (USE_DATABASE) {
+			item.setSaveDate(new Date());
+			item.setExpireDate(new Date());
 
-		session.getTransaction().begin();
-		session.save(item);
-		session.getTransaction().commit();
+			session.getTransaction().begin();
+			session.save(item);
+			session.getTransaction().commit();
+		} else {
+			item.setId(codeCounter); // temp hack
+			items.put(codeCounter, item);
+			codeCounter++;
+		}
 
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public CodeItem findItemById(int id) {
-		// TODO Guard from SQL injection
-		List<CodeItem> dataset = session.createQuery(
-				"from CodeItem where ID='" + Integer.toString(id) + "'").list();
-		return dataset.get(0);
+
+		if (USE_DATABASE) {
+			// TODO Guard from SQL injection
+			List<CodeItem> dataset = session.createQuery(
+					"from CodeItem where ID='" + Integer.toString(id) + "'")
+					.list();
+			return dataset.get(0);
+		} else {
+			return items.get(id);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<CodeItem> findAllItems() {
-		List<CodeItem> dataset = session.createQuery(
-				"from CodeItem where PRIVACY='Public'").list();
-		return dataset;
+		if (USE_DATABASE) {
+			List<CodeItem> dataset = session.createQuery(
+					"from CodeItem where PRIVACY='Public'").list();
+			return dataset;
+		} else {
+			return new ArrayList<>(items.values());
+		}
+
 	}
 
 	@Override
 	public void editCode(CodeItem item) {
-		// items.get(item.getId()).setText(item.getText());
+		if (USE_DATABASE) {
+		} else {
+			items.get(item.getId()).setText(item.getText());
+		}
 
 	}
 }
