76990a3a83674e1684f11a01ab8681761229fa49
unknown <sander.tiganik@gmail.com>
2014-03-16 18:23:08 +0200

    minor changes vol 2

--- src/main/webapp/script/login.js
+++ src/main/webapp/script/login.js
@@ -19,7 +19,7 @@ function objectify(t1){
 };
 
 $(document).ready(function () {
-	if(getCookie() == ""){
+	if(getCookie()){
 		var button = $("a#login");
 		button.html("Log Out");
 		button.click(function() {
