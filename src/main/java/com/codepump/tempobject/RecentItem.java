5cb0f7c39606de4a9d5af85ea99cd20e116c40a3
TKasekamp <toniskasekamp@gmail.com>
2014-03-19 15:47:28 +0200

    Recent items limited to 4

--- src/main/java/com/codepump/tempobject/RecentItem.java
+++ src/main/java/com/codepump/tempobject/RecentItem.java
@@ -18,7 +18,7 @@ import javax.persistence.NamedNativeQuery;
  */
 @Entity
 @NamedNativeQuery(name = "findRecentItemsInOrder", query = "select c.code_id, c.code_name,  c.code_language, "
-		+ "c.created_date, w.user_name, w.user_id FROM CodeItem as c JOIN webapp_user as w on w.user_id = c.user_id where c.privacy = 'Public' ORDER BY c.created_date DESC", resultClass = RecentItem.class)
+		+ "c.created_date, w.user_name, w.user_id FROM CodeItem as c JOIN webapp_user as w on w.user_id = c.user_id where c.privacy = 'Public' ORDER BY c.created_date DESC LIMIT 4", resultClass = RecentItem.class)
 public class RecentItem implements Serializable {
 	/**
 	 * 
