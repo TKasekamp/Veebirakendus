68b6178fdf468a834aa065c228b84b2aa0e29152
TKasekamp <toniskasekamp@gmail.com>
2014-03-26 13:42:55 +0200

    Terrible hack
    Jetty closes websocket automatically in about 1 minute. So I have told
    it to keep it open for 10 hours. That should be enough.

--- src/main/java/com/codepump/socket/RecentSocket.java
+++ src/main/java/com/codepump/socket/RecentSocket.java
@@ -9,44 +9,45 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import com.codepump.servlet.RecentSocketController;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @WebSocket
 public class RecentSocket {
 
-    private final RecentSocketController controller;
-    private Session session;
+	private final RecentSocketController controller;
+	private Session session;
 
-    public RecentSocket(RecentSocketController controller) {
-        this.controller = controller;
-    }
-
-    public void send(String message) throws IOException {
-        if (session.isOpen()) {
-            session.getRemote().sendString(message, null);
-        }
-    }
-
-    @OnWebSocketConnect
-    public void onOpen(Session session) {
-        this.session = session;
-        controller.getSockets().add(this);
-       try {
-		send( controller.onStartLoad());
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public RecentSocket(RecentSocketController controller) {
+		this.controller = controller;
 	}
-    }
 
-    @OnWebSocketClose
-    public void onClose(int status, String message) {
-        controller.getSockets().remove(this);
-    }
+	public void send(String message) throws IOException {
+		if (session.isOpen()) {
+			session.getRemote().sendString(message, null);
+		}
+	}
 
-    @OnWebSocketMessage
-    public void onMessage(Session session, String message) throws IOException {
-        System.out.println("received on websocket: " + message);
-    }
-    
-    
+	@OnWebSocketConnect
+	public void onOpen(Session session) {
+		this.session = session;
+		controller.getSockets().add(this);
+		session.setIdleTimeout(TimeUnit.HOURS.toMillis(10));
+		try {
+			send(controller.onStartLoad());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@OnWebSocketClose
+	public void onClose(int status, String message) {
+		controller.getSockets().remove(this);
+	}
+
+	@OnWebSocketMessage
+	public void onMessage(Session session, String message) throws IOException {
+		System.out.println("received on websocket: " + message);
+	}
+
 }
