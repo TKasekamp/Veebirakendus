fd77c7a582b1b0445f06780d10ba84fe98fb037a
TKasekamp <toniskasekamp@gmail.com>
2014-05-09 10:36:06 +0300

    Search samples for server memory
    Small fix to HTML

--- src/main/java/com/codepump/service/nodatabase/SearchServiceNoDB.java
+++ src/main/java/com/codepump/service/nodatabase/SearchServiceNoDB.java
@@ -1,5 +1,11 @@
 package com.codepump.service.nodatabase;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.codepump.data.CodeItem;
+import com.codepump.data.User;
 import com.codepump.service.SearchService;
 import com.codepump.tempobject.SearchContainer;
 
@@ -11,8 +17,11 @@ public class SearchServiceNoDB implements SearchService {
 	@Override
 	public SearchContainer searchDatabaseFuzzy(String query, int limit,
 			int offset, String sortField) {
-		// TODO Auto-generated method stub
-		return null;
+		List<CodeItem> results = new ArrayList<>();
+		for (int i = 0; i < 30; i++) {
+			results.add(new CodeItem(i,"Test code "+ Integer.toString(i),"This is sample text", "Sander-language", "Public", new Date(), new Date(), new User(i, "Sander", "bla", "")));
+		}
+		return new SearchContainer(results.size(), results);
 	}
 
 }
