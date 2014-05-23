5f174457b3746991f16ec2d634f8769475bb6859
Juhan-Rasmus Risti <juhan@risti.eu>
2014-05-23 11:13:11 +0300

    "Refactored" cache script.

---
+++ src/main/webapp/script/codeCache.js
@@ -0,0 +1,18 @@
+window.onbeforeunload = cacheCode;
+
+function cacheCode(){
+	var title = $('#codename').val();
+	var text = $('#codearea').val();
+	localStorage.setItem('title', title);
+	localStorage.setItem('text', text);
+	return null;
+}
+
+function checkCodeCache(){
+	if(localStorage.getItem('text') || localStorage.getItem('title')) {
+		$('#codename').val(localStorage.getItem('title'));
+		$('#codearea').val(localStorage.getItem('text'));
+	}
+}
+
+checkCodeCache();
